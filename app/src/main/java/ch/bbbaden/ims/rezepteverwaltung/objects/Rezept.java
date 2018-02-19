5e2da1fd1cd2343ab4f530ea3670f6d0cc87d46f
Noah.Schaub <noah.schaub@stud.bbbaden.ch>
2018-02-19 10:22:51 +0100

    19.02.2018 leere Menu und Rezept Activity erstellt Rezept object ertstellt Toaster Klasse zum ausgeben von Toasts erstellt

---
+++ app/src/main/java/ch/bbbaden/ims/rezepteverwaltung/objects/Rezept.java
@@ -0,0 +1,28 @@
+package ch.bbbaden.ims.rezepteverwaltung.objects;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Noah on 19.02.2018.
+ */
+
+public class Rezept {
+    @Getter
+    @Setter
+    private String rezeptName;
+
+    @Getter
+    @Setter
+    private List<List<String>> rezeptZutaten;
+
+    @Getter
+    @Setter
+    private String rezeptZubereitung;
+
+    @Getter
+    @Setter
+    private String rezeptDauer;
+}
---
+++ app/src/main/java/ch/bbbaden/ims/rezepteverwaltung/services/Toaster.java
@@ -0,0 +1,15 @@
+package ch.bbbaden.ims.rezepteverwaltung.services;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Noah on 19.02.2018.
+ */
+
+public class Toaster {
+    public Toaster(Context context,String toToast,int duration  ) {
+        Toast toast = Toast.makeText(context, toToast, duration);
+        toast.show();
+    }
+}
